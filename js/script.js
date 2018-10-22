34e728fa69dc30d02a018dce03e0b974a7c6ae88
Vikkkkk <vicguo.99@gmail.com>
2018-10-21 20:08:21 -0700

    initial ajax

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
-  // js code
-  console.log('Hello worldz');
+  $('#select-section').on('change', function() {
+    // event.preventDefault();
+
+    let url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+    url +=
+      '?' +
+      $.param({
+        'api-key': 'e9a1e979e8aa462e9143ac6fbdaa8557'
+      });
+
+    $.ajax({
+      method: 'GET',
+      url: url,
+      dataType: 'JSON'
+    })
+      .done(function(data) {
+        // console.log(data);
+        // console.log(data.results);
+
+        //loop through data.results
+        $.each(data.results, function(key, value) {
+          $('.list').append('<li>' + value.title + '</li>');
+        });
+      })
+
+      .fail(function() {
+        console.log('fail');
+      });
+  });
 });
